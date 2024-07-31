ef362b0b5ad453b05a3b566154fc0bd143380723
Matt <matthieu.coisnon@decathlon.com>
2024-07-31 15:58:02 +0200

    doc(swagger): optimize doc api

--- routes/routes.go
+++ routes/routes.go
@@ -6,25 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// SetupRouter sets up the API routes
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	golfGroup := r.Group("/v1/golfs")
+	v1 := r.Group("/v1")
 	{
-		golfGroup.GET("/", golf.GetGolfs)
-		golfGroup.GET("/:id", golf.GetGolfByID)
-		golfGroup.POST("/", golf.CreateGolf)
-		golfGroup.PATCH("/:id", golf.UpdateGolf)
-		golfGroup.DELETE("/:id", golf.DeleteGolf)
-	}
+		// Golf routes
+		golfGroup := v1.Group("/golfs")
+		{
+			golfGroup.GET("/", golf.GetGolfs)
+			golfGroup.GET("/:id", golf.GetGolfByID)
+			golfGroup.POST("/", golf.CreateGolf)
+			golfGroup.PATCH("/:id", golf.UpdateGolf)
+			golfGroup.DELETE("/:id", golf.DeleteGolf)
+		}
 
-	courseGroup := r.Group("/v1/courses")
-	{
-		courseGroup.GET("/", course.GetCourses)
-		courseGroup.GET("/:id", course.GetCourseByID)
-		courseGroup.POST("/", course.CreateCourse)
-		courseGroup.PATCH("/:id", course.UpdateCourse)
-		courseGroup.DELETE("/:id", course.DeleteCourse)
+		// Course routes
+		courseGroup := v1.Group("/courses")
+		{
+			courseGroup.GET("/", course.GetCourses)
+			courseGroup.GET("/:id", course.GetCourseByID)
+			courseGroup.POST("/", course.CreateCourse)
+			courseGroup.PATCH("/:id", course.UpdateCourse)
+			courseGroup.DELETE("/:id", course.DeleteCourse)
+		}
 	}
 
 	return r
