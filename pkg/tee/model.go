239e3a95df401094daeeee18225396939e38499f
Matt <matthieu.coisnon@decathlon.com>
2024-08-03 01:26:42 +0200

    feat(tee): add example to tee model

--- pkg/tee/model.go
+++ pkg/tee/model.go
@@ -8,9 +8,9 @@ import (
 // @Description Model for a tee
 type Model struct {
 	common.Base
-	Color    string `gorm:"type:varchar(50);not null"`
-	Distance int    `gorm:"not null"`
-	HoleID   string `gorm:"type:uuid;not null"`
+	Color    string `gorm:"type:varchar(50);not null" example:"White"`
+	Distance int    `gorm:"not null" example:"353"`
+	HoleID   string `gorm:"type:uuid;not null" example:"123e4567-e89b-12d3-a456-426614174000"`
 } // @name Model
 
 // TableName sets the insert table name for this struct type
