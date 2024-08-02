c37438b2bee25db87dfd47e1fed5f9b413eeb007
Matt <matthieu.coisnon@decathlon.com>
2024-08-03 01:23:19 +0200

    feat(course): add example to course model

--- pkg/course/model.go
+++ pkg/course/model.go
@@ -8,11 +8,11 @@ import (
 // @Description Model for a golf course
 type Model struct {
 	common.Base
-	Name         string `json:"name" gorm:"type:varchar(255);not null"`
-	NumHoles     int    `json:"num_holes" gorm:"not null"`
-	Compact      bool   `json:"compact" gorm:"default:false"`
-	PitchAndPutt bool   `json:"pitch_and_putt" gorm:"default:false"`
-	GolfID       string `json:"golf_id" gorm:"type:uuid;not null"`
+	Name         string `json:"name" gorm:"type:varchar(255);not null" example:"L’Albatros"`
+	NumHoles     int    `json:"num_holes" gorm:"not null" example:"18"`
+	Compact      bool   `json:"compact" gorm:"default:false" example:"false"`
+	PitchAndPutt bool   `json:"pitch_and_putt" gorm:"default:false" example:"false"`
+	GolfID       string `json:"golf_id" gorm:"type:uuid;not null" example:"123e4567-e89b-12d3-a456-426614174000"`
 } // @Name Course
 
 // TableName sets the insert table name for this struct type
