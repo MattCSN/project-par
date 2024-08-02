c8de950ef35bef4d46b1558e1bf839a64976b4e7
Matt <matthieu.coisnon@decathlon.com>
2024-08-03 01:26:33 +0200

    feat(course): add example to hole model

--- pkg/hole/model.go
+++ pkg/hole/model.go
@@ -8,9 +8,9 @@ import (
 // @Description Model for a hole
 type Model struct {
 	common.Base
-	HoleNumber int    `gorm:"not null"`
-	Par        int    `gorm:"not null"`
-	CourseID   string `gorm:"type:uuid;not null"`
+	HoleNumber int    `gorm:"not null" example:"1"`
+	Par        int    `gorm:"not null" example:"4"`
+	CourseID   string `gorm:"type:uuid;not null" example:"123e4567-e89b-12d3-a456-426614174000"`
 } // @name Hole
 
 // TableName sets the insert table name for this struct type
